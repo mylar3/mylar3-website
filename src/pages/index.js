83efbb76a682f11b1252cf33b36c6c0ae8b16bc8
evilhero <909424+evilhero@users.noreply.github.com>
2023-08-22 21:34:48 -0400

    layout updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,9 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero', styles.heroBanner)}>
       <div className="container">
+        <img src="img/mylarlogo.png" width="100px" height="100px"/>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
